fd03ff66cf227d320dda5d44532e05997478a501
Andrew Barr <andrew@foxtail.consulting>
2020-11-04 09:14:02 +1000

    using tailwind native purge

--- assets/postcss.config.js
+++ assets/postcss.config.js
@@ -1,22 +1,9 @@
 const { purge } = require('tailwindcss/stubs/defaultConfig.stub');
-const purgecss = require('@fullhuman/postcss-purgecss')(
-  {
-    content: [
-      "../**/*.html.eex",
-      "../**/*.html.leex",
-      "../**/views/**/*.ex",
-      "../**/live/**/*.ex",
-      "./js/**/*.js"
-    ],
-    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-  }
-)
 
 module.exports = {
   plugins: [
     require('postcss-import'),
     require('tailwindcss'),
-    require('autoprefixer'),
-    ...(process.env.NODE_ENV === "production" ? [purgecss] : [])    
+    require('autoprefixer')
   ]
 }
--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -3,7 +3,13 @@ module.exports = {
     // removeDeprecatedGapUtilities: true,
     // purgeLayersByDefault: true,
   },
-  purge: [],
+  purge: [
+      "../**/*.html.eex",
+      "../**/*.html.leex",
+      "../**/views/**/*.ex",
+      "../**/live/**/*.ex",
+      "./js/**/*.js"
+  ],
   theme: {
     extend: {},
   },
