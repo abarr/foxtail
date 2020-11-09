b628ba96484d6772a489a4b224ce268af91dabe6
Andrew Barr <andrew@foxtail.consulting>
2020-11-09 12:49:04 +1000

    phoenix_live_view

--- mix.exs
+++ mix.exs
@@ -39,7 +39,7 @@ defmodule Foxtail.MixProject do
       {:phoenix_live_view, "~> 0.14.6"},
       {:floki, ">= 0.27.0", only: :test},
       {:phoenix_html, "~> 2.11"},
-      {:phoenix_live_reload, "~> 1.2", only: :dev},
+      {:phoenix_live_reload, "~> 1.3", only: :dev},
       {:phoenix_live_dashboard, "~> 0.3 or ~> 0.2.9"},
       {:telemetry_metrics, "~> 0.4"},
       {:telemetry_poller, "~> 0.4"},
